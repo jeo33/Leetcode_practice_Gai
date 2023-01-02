0f649e746f2d8141e03359c3cab4686dde61beea
junpeng gai <34943011+jeo33@users.noreply.github.com>
2023-01-02 14:04:01 -0500

    Create src.java

---
+++ easy question/69. Sqrt(x)/src.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        long i=0;
+        while(i*i<=x)
+        {
+            i++;
+        }
+        return (int)(i-1);
+        
+    }
+    
+    
+}
